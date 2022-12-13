ccc9683fa50b57b4bf1012587f3e9e9f95b632be
Jorge <jbarretolarrosa@gmail.com>
2022-12-13 13:30:29 -0300

    Add missing typos

--- client/src/Utils/CustomHooks.ts
+++ client/src/Utils/CustomHooks.ts
@@ -1,14 +1,12 @@
 import { useCallback, useState } from "react";
 
 // Parameter is the boolean, with default "false" value
-const useToggle = (initialState = false) => {
+const useToggle = (initialState: boolean = false): [boolean, any] => {
   // Initialize the state
-  const [state, setState] = useState(initialState);
-
-  // Define and memorize toggler function in case we pass down the component,
+  const [state, setState] = useState<boolean>(initialState);
+  // Define and memorize toggler function in case we pass down the comopnent,
   // This function change the boolean value to it's opposite value
-  const toggle = useCallback(() => setState((state) => !state), []);
-
+  const toggle = useCallback((): void => setState((state) => !state), []);
   return [state, toggle];
 };
 
