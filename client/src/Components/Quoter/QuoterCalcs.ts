7a0f1e967dfd66f5c317ed924213c1ee8204cb5e
Jorge <jbarretolarrosa@gmail.com>
2022-12-23 17:02:06 -0300

    Rework funcs and typos to get correct information

--- client/src/Components/Quoter/QuoterCalcs.ts
+++ client/src/Components/Quoter/QuoterCalcs.ts
@@ -1,61 +1,40 @@
-import { bagCostsSetState, quoterBackEndInfo } from "Utils/Interfaces";
+import { AxiosResponse } from "axios";
+import { bagInfoSetState } from "Utils/Interfaces";
 
 /* Calculate the cost of the cord used. */
-const cordCostCalc = (
-  state: bagCostsSetState,
-  setState: React.Dispatch<React.SetStateAction<bagCostsSetState>>,
-  bagWidth: number,
-  quoterPrices: quoterBackEndInfo
-) => {
+const cordCostCalc = (bagInfo: bagInfoSetState, quoterPrices: AxiosResponse<any, any> | undefined) => {
   /* *4 bcuz the cord goes 4 times thru the width,
    +20 includes the cord used out of the width & 
    cordPrice / 100 bcuz we need centimeters and the price is store in meters */
-  const cost = Math.ceil((bagWidth * 4 + 20) * (quoterPrices.cordPrice / 100));
-  setState({ ...state, cordCost: cost });
+  return Math.ceil(
+    (bagInfo.bagWidth * 4 + 20) * (quoterPrices?.data[0].cordPrice / 100) * bagInfo.bagQuantity
+  );
 };
 /* Thread used cant be calculated in centimeters. 
 For that we calculate $40 for every 50 bags */
-const threadCostCalc = (
-  bagQuantity: number,
-  state: bagCostsSetState,
-  setState: React.Dispatch<React.SetStateAction<bagCostsSetState>>
-) => {
-  const cost = Math.ceil(bagQuantity / 50) * 40;
-  setState({ ...state, threadCost: cost });
+const threadCostCalc = (bagInfo: bagInfoSetState) => {
+  return Math.ceil(bagInfo.bagQuantity / 50) * 40;
 };
 
 /* Calculate grifa's cost depending on the bags's amount */
-const grifaCostCalc = (
-  state: bagCostsSetState,
-  setState: React.Dispatch<React.SetStateAction<bagCostsSetState>>,
-  bagQuantity: number,
-  quoterPrices: quoterBackEndInfo
-) => {
-  const cost = bagQuantity * quoterPrices.grifaPrice;
-  setState({ ...state, grifaCost: cost });
+const grifaCostCalc = (bagInfo: bagInfoSetState, quoterPrices: AxiosResponse<any, any> | undefined) => {
+  return bagInfo.bagQuantity * quoterPrices?.data[0].grifaPrice;
 };
 
-const clothCostCalc = (
-  state: bagCostsSetState,
-  setState: React.Dispatch<React.SetStateAction<bagCostsSetState>>,
-  bagQuantity: number,
-  bagWidth: number,
-  bagLength: number,
-  quoterPrices: quoterBackEndInfo
-) => {
-  /* Calculate how much bags fit using the bag width over the roll width*/
-  const bagWxRollW = Math.floor(quoterPrices.cloth[0].rollWidth / bagWidth);
-  /*Calculate how much bags fit using the bag length over the roll width*/
-  const bagLxRollW = Math.floor(quoterPrices.cloth[0].rollWidth / bagLength);
+const clothCostCalc = (bagInfo: bagInfoSetState, quoterPrices: AxiosResponse<any, any> | undefined) => {
+  /* Calculate how much bags fit using the bag width over the roll width -- +3 is the cloth excess for the sewing*/
+  const bagWxRollW = Math.floor(quoterPrices?.data[0].cloth[0].rollWidth / (bagInfo.bagWidth + 3));
+  /*Calculate how much bags fit using the bag length over the roll width -- x 2 is for the 2 sides u need for a bag and the +5 is for the cloth excess for the sewing*/
+  const bagLxRollW = Math.floor(quoterPrices?.data[0].cloth[0].rollWidth / (bagInfo.bagLength * 2 + 5));
   /* Checks if its better to cut bags over the width or the length */
-  const clothCentimetersW = (bagLength * bagQuantity) / bagWxRollW;
-  const clothCentimetersL = (bagWidth * bagQuantity) / bagLxRollW;
-  if (clothCentimetersW > clothCentimetersL) {
-    const cost = clothCentimetersW * (quoterPrices.cloth[0].price / 100);
-    setState({ ...state, clothCost: cost });
+  const clothCentimetersW = Math.ceil(((bagInfo.bagLength * 2 + 5) * bagInfo.bagQuantity) / bagWxRollW);
+  const clothCentimetersL = Math.ceil(((bagInfo.bagWidth + 3) * bagInfo.bagQuantity) / bagLxRollW);
+  console.log(bagInfo, bagWxRollW, bagLxRollW, clothCentimetersW, clothCentimetersL);
+
+  if (clothCentimetersW < clothCentimetersL) {
+    return Math.ceil(clothCentimetersW * (quoterPrices?.data[0].cloth[0].price / 100));
   } else {
-    const cost = clothCentimetersL * (quoterPrices.cloth[0].price / 100);
-    setState({ ...state, clothCost: cost });
+    return Math.ceil(clothCentimetersL * (quoterPrices?.data[0].cloth[0].price / 100));
   }
 };
 
