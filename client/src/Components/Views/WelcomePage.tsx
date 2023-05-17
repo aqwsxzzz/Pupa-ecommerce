8456b967fa732d19f30e885b09f06f0389c23724
Jorge Barreto <jbarretolarrosa@gmail.com>
2023-05-17 14:46:21 -0300

    Add client and admin buttons with navigate funcs. Finish basic design.

--- client/src/Components/Views/WelcomePage.tsx
+++ client/src/Components/Views/WelcomePage.tsx
@@ -1,17 +1,12 @@
-import {
-  Box,
-  Button,
-  FormControl,
-  Image,
-  Text,
-  useMediaQuery,
-} from "@chakra-ui/react";
+import { Box, Button, Flex, FormControl, HStack, Image, Text, useMediaQuery } from "@chakra-ui/react";
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import logo from "../../Images/2.jpeg";
 
 export const WelcomePage: React.FC = () => {
-  const [isLargerThan768] = useMediaQuery("(min-width: 913px");
-  const [localTime, setLocalTime] = useState(new Date().getHours());
+  const navigate = useNavigate();
+  const [isLargerThan913] = useMediaQuery("(min-width: 913px");
+  const [localTime] = useState(new Date().getHours());
 
   let message = "";
   if (localTime > 5 && localTime < 13) {
@@ -20,20 +15,37 @@ export const WelcomePage: React.FC = () => {
     message = "Buenas tardes!";
   } else message = "Buenas noches!";
 
-  console.log(localTime);
+  return isLargerThan913 ? null : (
+    <Flex flexDir={"column"} bgColor={"#f0d3e9"}  h={"912px"} w={"100%"} justify={"space-evenly"} >
+      <Box alignSelf={"center"} mt={"-16"}>
 
-  return isLargerThan768 ? null : (
-    <Box bgColor={"#f0d3e9"} h={"912px"}>
-      <Box>
-        <Box borderRadius={"50%"} style={{ overflow: "hidden" }} w={24} h={24}>
-          <Image src={logo} />
+      <Box
+          my={"auto"}
+          w={64}
+          h={64}
+          bgColor={"white"}
+          borderRadius={"50%"}
+          borderWidth={1}
+          borderColor={"black"}
+          style={{ overflow: "hidden" }}
+
+        >
+          <Flex mt={7} ml={3} >
+            <Image src={logo} w={48} m={"auto"} />
+          </Flex>
         </Box>
-        <Text>{message}</Text>
-        <FormControl>
-          <Button>Client@</Button>
-          <Button>Administrador</Button>
+          </Box>
+      <Flex flexDir={"column"} justify={"center"} w={"100%"} align={"center"}>
+        <Text color={"#B83280"} fontSize={"xxx-large"} mt={"-28"} mb={"4"}>{message}</Text>
+        <Box>
+        <FormControl >
+          <HStack spacing={1}>
+          <Button colorScheme={"pink"} textColor={"#B7CD6A"} onClick={() => navigate("/home")}>Client@</Button>
+          <Button bgColor={"pink.500"} textColor={"#B7CD6A"} onClick={() => navigate("/admin")}>Administrador</Button>
+          </HStack>
         </FormControl>
-      </Box>
-    </Box>
+        </Box>
+      </Flex>
+    </Flex>
   );
 };
